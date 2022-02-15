f4219647547b5589598f23abc2916901dee02a7c
Borbás Mátyás <25402691+borbasmatyas@users.noreply.github.com>
2022-02-15 14:05:40 +0100

    Update download-arukereso.php

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+
+$remote_file    = "https://fotoplus.hu/public/catalogues/arukereso.csv";
+$remote_host    = "http://fotoplus.hu";
+
+?>
---
+++ read-arukereso.php
@@ -0,0 +1,18 @@
+<?php
+
+$url = $remote_file;
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_VERBOSE, 1);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_AUTOREFERER, false);
+curl_setopt($ch, CURLOPT_REFERER, $remote_host);
+curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
+curl_setopt($ch, CURLOPT_HEADER, 0);
+$result = curl_exec($ch);
+curl_close($ch);
+
+print_r($result); // prints the contents of the collected file before writing..
+
+
+?>
