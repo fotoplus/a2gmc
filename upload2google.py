edb61d06dc3ee244f783564747d680bb0071fbc3
Borbás Mátyás <25402691+borbasmatyas@users.noreply.github.com>
2022-02-15 17:59:44 +0100

    Update upload2google.py

--- upload2google.py
+++ upload2google.py
@@ -1,13 +1,15 @@
-import sys
+#import sys
 import pysftp
 
-sys.path.append('/etc/credentials/sftp-google-merchant-center.py') 
+file1 = "/etc/credentials/sftp-google-merchant-center.py"
+from file1 import *  
+
+#sys.path.append('/etc/credentials/sftp-google-merchant-center.py') 
 
 srv = pysftp.Connection(host=SFTP_server, username=SFTP_user,
 password=SFTP_port,port=SFTP_port,log="./temp/pysftp.log")
 
-with srv.cd('/'): #chdir to public
+with srv.cd('/'):
     srv.put('xml/fotoplus.xml') 
 
-# Closes the connection
 srv.close()
