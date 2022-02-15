4932b18b82bbb9b0ad42fc22f9c81c61194f1f14
Borbás Mátyás <25402691+borbasmatyas@users.noreply.github.com>
2022-02-15 14:24:40 +0100

    Update

--- a2gmc.php
+++ a2gmc.php
@@ -2,6 +2,7 @@
 
 include('config.php');
 include('read-arukereso.php');
+include('parse-csv.php');
 include('convert2xml.php');
 
 ?>
---
+++ parse-csv.php
@@ -0,0 +1,61 @@
+<?php
+/*
+str_getcsv(
+    string $string,
+    string $separator = ",",
+    string $enclosure = "\"",
+    string $escape = "\\"
+): array
+*/
+
+#$csv = array_map('str_getcsv', file('arukereso.csv')); 
+
+#print_r($csv);
+
+/*
+$data_structure = array(
+    0   => 'identifier',
+    1   => 'code',
+    2   => 'category',
+    3   => 'manufacturer',
+    4   => 'name',
+    5   => 'description',
+    6   => 'net_price',
+    7   => 'price',
+    8   => 'image_url',
+    9   => 'product_url',
+    10  => 'delivery_time',
+    11  => 'delivery_cost'
+);
+
+
+function process_csv($file) {
+
+    $file = fopen($file, "r");
+    $data = array();
+
+    while (!feof($file)) {
+        $data[] = fgetcsv($file, null, ';');
+    }
+
+    fclose($file);
+    return $data;
+}
+
+$file = "arukereso.csv";
+$array = process_csv($file);
+*/
+
+$file = "arukereso.csv";
+
+$rows = array_map(function($v){return str_getcsv($v, ";");}, explode("\n", $result));
+#$rows = array_map(function($v){return str_getcsv($v, ";");}, file($file,FILE_SKIP_EMPTY_LINES));
+$header = array_shift($rows);
+$csv = array();
+foreach ($rows as $row) {
+    $csv[] = array_combine($header, $row);
+}
+
+print_r($csv);
+
+?>
