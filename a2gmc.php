365e9fe4b220b8612181582e6ebaf1473f2fac6a
Borbás Mátyás <25402691+borbasmatyas@users.noreply.github.com>
2022-02-16 14:22:08 +0100

    Update a2gmc.php

--- a2gmc.php
+++ a2gmc.php
@@ -5,7 +5,7 @@ include('read-arukereso.php');
 include('parse-csv.php');
 include('convert2xml.php');
 
-$command = escapeshellcmd('python upload2google.py');
+$command = escapeshellcmd('python3 upload2google.py');
 $output = shell_exec($command);
 echo $output;
 
