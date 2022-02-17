af32d015790c61a449a43646472368b02115d2fc
Borbás Mátyás <25402691+borbasmatyas@users.noreply.github.com>
2022-02-17 10:51:06 +0100

    Update convert2xml.php

--- convert2xml.php
+++ convert2xml.php
@@ -47,6 +47,7 @@ foreach ($csv as $item) {
 
 		$g_name = str_replace('&amp,', '&', $item['name']);
 		$g_description = str_replace('&amp,', '&', $item['description']);
+		$g_brand = str_replace('&amp,', '&', $item['manufacturer']);
 
 		$g_name = str_replace(',', '', $g_name);
 		$g_description = str_replace(',', '', $g_description);
